477765c7fee2cdddd4f5ffc0ed35a1d2501fef85
karmakersagar <karmakersagar25@gmail.com>
2023-05-09 00:39:52 +0600

    User Authentication problem solved

--- duits/userRegistration/views.py
+++ duits/userRegistration/views.py
@@ -139,42 +139,51 @@ def success(request):
 def profile(request) :
     return render(request,'registration/profile.html')
 def signup(request):
-    if request.method == 'POST':
-        # first_name = request.POST['first_name']
-        # last_name = request.POST['first_name']
-        
-        registration_no = request.POST.get('registration_no')
-        email = request.POST.get('email')        # dept_name = request.POST['dept_name']
-        password = request.POST.get('password')
-        confirm_password = request.POST.get('confirm_password')
-        print(password,confirm_password)
-        # roll = request.POST['roll']
-        # session = request.POST['session']
+ if request.method == 'POST':
+    registration_no = request.POST.get('registration_no')
+    email = request.POST.get('email')
+    password = request.POST.get('password')
+    confirm_password = request.POST.get('confirm_password')
+    print(password, confirm_password)
 
-        if password == confirm_password:
-            if CustomUser.objects.filter(email=email).exists():
-                messages.error(request, 'email already exists')
-                return redirect('/signup')
-            elif CustomUser.objects.filter(registration_no=registration_no).exists():
-                messages.error(
-                    request, 'you are already registered by your registration number')
-                return redirect('/signup')
-            else:
-                user = CustomUser(
-                    registration_no=registration_no, email=email, password=make_password(password),date_joined=datetime.datetime,last_login=datetime.time,is_active=True,is_admin=False,is_superuser=False,is_staff=False)
-                user.save()
-                authenticate_user = authenticate(registration_no=registration_no,password=password)
-                authenticate_user.save()
-                
-                messages.success(request, 'user saved successfully')
-
-        else:
-            messages.error(request, 'password do not match')
+    if password == confirm_password:
+        if CustomUser.objects.filter(email=email).exists():
+            messages.error(request, 'email already exists')
             return redirect('/signup')
-    else:
-        return render(request, 'registration/signup.html')
+        elif CustomUser.objects.filter(registration_no=registration_no).exists():
+            messages.error(request, 'you are already registered by your registration number')
+            return redirect('/signup')
+        else:
+            user = CustomUser(
+                registration_no=registration_no,
+                email=email,
+                password=make_password(password),
+                date_joined=datetime.datetime.now(),
+                last_login=datetime.datetime.now(),
+                is_active=True,
+                is_admin=False,
+                is_superuser=False,
+                is_staff=False,
+            )
+            user.save()
 
-    return render(request, 'registration/login.html')
+            # Authenticate and login user
+            authenticated_user = authenticate(request, registration_no=registration_no, password=password)
+            if authenticated_user is not None:
+                login(request, authenticated_user)
+                messages.success(request, 'User authenticated and logged in successfully')
+            else:
+                messages.error(request, 'Unable to authenticate user')
+
+            return redirect('/')
+
+    else:
+        messages.error(request, 'password do not match')
+        return redirect('/signup')
+
+ else:
+    return render(request, 'registration/signup.html')
+
 
 def login_view(request):
     if request.method == 'POST':
